c96ac523d5f66556c2e224e5dd84fc73d6cc1ad8
daniyakobi <nadeev.danil2016@yandex.ru>
2020-08-11 14:39:57 +0300

    Create: recommendation section and responsive.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
 $(document).ready(function(){
+  // Меню бургер
   $('.menu-burger').click(function(event){
     $('.menu-burger').toggleClass('active-burger');
     $('.mobile-menu').toggleClass('active-menu');
     $('body').toggleClass('lock');
+  });
+  // Табы
+  $('.recommendation').click(function() {
+    var id = $(this).attr('data-tab'),
+      content = $('.recommended__use[data-tab="'+ id +'"]');
+    $('.recommendation.active').removeClass('active');
+    $(this).addClass('active');
+    $('.recommended__use.tab-active').removeClass('tab-active'); 
+    content.addClass('tab-active');
   });
 });
